d8c7a6f2686ced9d77ef92ab22e6313648da3d0e
alextxu <38475194+alextxu@users.noreply.github.com>
2020-08-16 00:20:31 -0400

    Fixed undo bug
    Fixed the bug that occured when the user tried to undo a deletion.

--- Assets/Scripts/ARTapToPlaceObject.cs
+++ Assets/Scripts/ARTapToPlaceObject.cs
@@ -144,7 +144,11 @@ public class ARTapToPlaceObject : MonoBehaviour
             if (hit.transform.name == "NaturePack_Grass1" || hit.transform.name == "default"  || hit.transform.name == "Plane.001")
             {
                 Debug.LogWarning("name works");
-                hit.transform.gameObject.SetActive(false);
+
+                GameObject currentObject = hit.transform.gameObject;
+                currentObject.SetActive(false);
+                objectsChanged.Push(currentObject);
+                changes.Push(0);
                 clearRedoStack();
 
                 //Destroy(hit.transform.gameObject);
