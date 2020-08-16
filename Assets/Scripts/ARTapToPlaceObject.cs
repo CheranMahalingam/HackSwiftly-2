09c24388bc0ea48fa8091033b37c5c2a4542381f
alextxu <38475194+alextxu@users.noreply.github.com>
2020-08-16 15:16:21 -0400

    Update ARTapToPlaceObject.cs

--- Assets/Scripts/ARTapToPlaceObject.cs
+++ Assets/Scripts/ARTapToPlaceObject.cs
@@ -43,6 +43,7 @@ public class ARTapToPlaceObject : MonoBehaviour
     public Animator animator6;
     public Animator trashAnimator;
     private int cnt = 0;
+<<<<<<< HEAD
     private Text text;
     private string userCountry; 
 
@@ -60,6 +61,10 @@ public class ARTapToPlaceObject : MonoBehaviour
     private int PrevType = 0;
     private bool MenuState = false;
 
+=======
+    public Text carbonFootprint;
+    private Text text; 
+>>>>>>> parent of 9063ace... Added carbon footprint calculations for add, delete, and move.
 
     // Hardcoding researched data
     private string[] countries = { "Africa", "Algeria", "Argentina", "Asia", "Asia (excl. China & India)", "Australia", "Austria", "Azerbaijan", "Bangladesh", "Belarus", "Belgium", "Brazil", "Bulgaria", "Canada", "Chile", "China", "Colombia", "Croatia", "Cyprus", "Czech Republic", "Denmark", "EU-27", "EU-28", "Ecuador", "Egypt", "Estonia", "Europe", "Europe (excl. EU-27)", "Europe (excl. EU-28)", "Finland", "France", "Germany", "Greece", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Japan", "Kazakhstan", "Kuwait", "Latvia", "Lithuania", "Luxembourg", "Macedonia", "Malaysia", "Mexico", "Morocco", "Netherlands", "New Zealand", "North America", "North America (excl. USA)", "Norway", "Oceania", "Oman", "Pakistan", "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russia", "Saudi Arabia", "Singapore", "Slovakia", "Slovenia", "South Africa", "South America", "South Korea", "Spain", "Sri Lanka", "Sweden", "Switzerland", "Taiwan", "Thailand", "Trinidad and Tobago", "Turkey", "Turkmenistan", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "Uzbekistan", "Venezuela", "Vietnam", "World" };
